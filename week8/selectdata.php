35da1a9ac1318b4cb866b70d5deeea20b3eb734f
ndcatabona <ndcatabona@student.apc.edu.ph>
2024-02-16 02:56:03 +0800

    updated sql

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM mmmanlapaz_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM ndcatabona_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
